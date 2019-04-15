08c95d309216607b3a17f44cb55231f86ea49713
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-15 16:29:53 +0300

    Reverse integer string variant.

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,24 +1,20 @@
-def reverse(data):
-    k = len(data) - 1
-    data_str = str(data)
+def reverse(x):
+    k = len(str(x)) - 1
+    data_str = str(x)
     new_data = ''
-    if (2 ** 31) - 1 < int(data) < (-2) ** 31:
-        return 0
-    elif data_str[0] == '-':
+    if data_str[0] == '-':
         new_data = '-'
-        while k > 0 and data_str[k] == '0':
-            k -= 1
         while k > 0:
             new_data = new_data + data_str[k]
             k -= 1
-        print(new_data)
-    elif data_str[0] != '-':
-        while k > 0 and data_str[k] == '0':
-            k -= 1
+    else:
         while k >= 0:
             new_data = new_data + data_str[k]
             k -= 1
-        print(new_data)
+    if 2 ** 31 - 1 <= int(new_data) or int(new_data) <= (- 2) ** 31:
+        print(0)
+    else:
+        print(int(new_data))
 
 
 number = input("Please, write a number: ")
