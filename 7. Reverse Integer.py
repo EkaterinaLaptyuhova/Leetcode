3e98a04f04e333cc4cbc65a92bde9d1cfeaa0845
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-15 15:29:08 +0300

    Reverse integer

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,1 +1,25 @@
-# first task
+def reverse(data):
+    k = len(data) - 1
+    data_str = str(data)
+    new_data = ''
+    if (2 ** 31) - 1 < int(data) < (-2) ** 31:
+        return 0
+    elif data_str[0] == '-':
+        new_data = '-'
+        while k > 0 and data_str[k] == '0':
+            k -= 1
+        while k > 0:
+            new_data = new_data + data_str[k]
+            k -= 1
+        print(new_data)
+    elif data_str[0] != '-':
+        while k > 0 and data_str[k] == '0':
+            k -= 1
+        while k >= 0:
+            new_data = new_data + data_str[k]
+            k -= 1
+        print(new_data)
+
+
+number = input("Please, write a number: ")
+reverse(number)
