9c5369e0d87d3cf5ef6cf67f14fbf33ba048dba5
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-16 17:40:21 +0300

    Palindrome.

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        duplicate = x
+        y = 0
+        if x < 0:
+            return False
+
+        while x > 0:
+            y = y * 10 + (x % 10)
+            x = x // 10
+
+        if duplicate == y:
+            return True
+        else:
+            return False
