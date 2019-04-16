f65a90b63801e08a47288f3e33dce2a61d48282e
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-16 16:50:25 +0300

    Reverse integer, leetcode variant.

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -20,7 +20,7 @@ def reverse(x):
 
 number = input("Please, write a number: ")
 reverse(number)
-"""
+
 
 
 def reverse_int(x):
@@ -43,6 +43,29 @@ def reverse_int(x):
         print(0)
     else:
         print(y)
+"""
 
 
-reverse_int(1534236469)
+def reverse(self, x: int) -> int:
+    y = 0
+    k = 0
+    if x < 0:
+        x = x * (- 1)
+        k = 1
+    while x // 10 != 0:
+        if x % 10 == 0:
+            y = y * 10
+            x = x // 10
+        else:
+            y = y * 10 + (x % 10)
+            x = x // 10
+    y = y * 10 + x
+    if k == 1:
+        y = y * (- 1)
+    if 2 ** 31 - 1 <= y or y <= (- 2) ** 31:
+        return 0
+    else:
+        return y
+
+
+
