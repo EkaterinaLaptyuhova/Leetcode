fdbe22b1dbbcc70b2728c32c85fb87d7dc41c6a8
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-16 18:51:08 +0300

    412. Fizz Buzz

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        k = 1
+        str_represent = []
+        while k <= n:
+            if k % 3 == 0 and k % 5 == 0:
+                str_represent.append('FizzBuzz')
+                k += 1
+            elif k % 3 == 0:
+                str_represent.append('Fizz')
+                k += 1
+            elif k % 5 == 0:
+                str_represent.append('Buzz')
+                k += 1
+            else:
+                element = str(k)
+                str_represent.append(element)
+                k += 1
+        return str_represent
+
+
